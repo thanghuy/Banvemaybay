0d48d63143b730640d57cef47337c0b23b7d05ed
Huy Thang <54946880+thanghuy@users.noreply.github.com>
2019-11-08 08:15:03 +0700

    nhh
    lkhfds

--- src-vs1/app/controller/home.php
+++ src-vs1/app/controller/home.php
@@ -6,26 +6,12 @@
         }
         public function show()
         {
-            echo getCurURL();
+            echo "huythang";
         }
         public function product($a,$b)
         {
             echo $a."-----".$b;
             $this->views("product",null);
         }
-        function getCurURL()
-        {
-            if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {
-                $pageURL = "https://";
-            } else {
-            $pageURL = 'http://';
-            }
-            if (isset($_SERVER["SERVER_PORT"]) && $_SERVER["SERVER_PORT"] != "80") {
-                $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
-            } else {
-                $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
-            }
-            return $pageURL;
-
-        }
+    }
 ?>
