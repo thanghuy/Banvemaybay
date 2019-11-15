03b0fdc5b379419b7cc4b53dae85457a59067f4f
Ninh Ngoc Hieu <30309069+ninhngochieu@users.noreply.github.com>
2019-11-15 22:11:34 +0700

    fix

--- src-vs1/public/js/main.js
+++ src-vs1/public/js/main.js
@@ -1,15 +1,16 @@
 
 var sanpham = document.getElementById('detail-fight');
 var ve = document.getElementById("ticker");
-$('.bay').click(function() {
-    sanpham.style.display = 'flex';
-    ve.style.display = 'none';
-    $(".bay").addClass("hide-1");
-    $(".ve-1").removeClass("hide-2");
-    $(".show-1").addClass("active");
-    $(".show-1").addClass("active");
-    $(".show-2").removeClass("active");
+$('.bay').click(function(){
+	sanpham.style.display = 'flex';
+	ve.style.display = 'none';
+	$(".bay").addClass("hide-1");
+	$(".ve-1").removeClass("hide-2");
+	$(".show-1").addClass("active");
+	$(".show-1").addClass("active");
+	 $(".show-2").removeClass("active");
 })
+<<<<<<< HEAD
 $('.ve-1').click(function() {
     ve.style.display = 'flex';
     sanpham.style.display = 'none';
@@ -107,3 +108,13 @@ function check_sign_up() {
     return false;
 
 }
+=======
+$('.ve-1').click(function(){
+	ve.style.display = 'flex';
+	sanpham.style.display = 'none';
+	$(".ve-1").addClass("hide-2");
+	$(".bay").removeClass("hide-1");
+	$(".show-2").addClass("active");
+	$(".show-1").removeClass("active");
+})
+>>>>>>> parent of 163bd76... Merge branch 'master' of https://github.com/thanghuy/Banvemaybay
