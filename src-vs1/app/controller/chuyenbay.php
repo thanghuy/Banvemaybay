43eb871dad49990502361faa865fead101ddfb62
Huy Thang <54946880+thanghuy@users.noreply.github.com>
2019-11-17 11:35:05 +0700

    timkiem
    ohantrang

--- src-vs1/app/controller/chuyenbay.php
+++ src-vs1/app/controller/chuyenbay.php
@@ -1,11 +1,54 @@
 <?php
-    class chuyenbay extends Controller{
-        function ChuyenBay(){
-            $a = $this->model('MaybayModel');
-            $this->views("index_v",'chuyenbay_v',[
-                'Ho' => $a->getMaybay()
-            ]);
-            
+    class chuyenbay extends Controller
+	{
+		function tiemkiem()
+		{
+			// Get data from FORM
+			// $customRadioInline = (int)$_POST["customRadioInline"]; // 1: 1 chiều			// 2: khứ hồi
+			// $dateto = new MyDate($_POST["to-date"]);
+			// $numseat = $_POST["seat-class"];
+			// $fightfrom = $_POST["from-fight"];
+			// $fightto = $_POST["to-fight"];
+			// $tenchang = $fightfrom . "->" . $fightto;
+			// $datefrom = new MyDate($_POST["form-date"]);
+			
+			// Find result
+			$numpeople = (int)$_POST["nguoilon"] + (int)$_POST["treem"] + (int)$_POST["embe"];
+
+			$ngaydi = $_POST["from-date"];
+			if (strlen($ngaydi) == 0) {
+				$ngaydi = date("d-m-Y");
+			}
+			
+			$this->chontrang($_POST["from-fight"], $_POST["to-fight"], $ngaydi, $numpeople, 1);
+		}
+		
+		function chontrang($diemdi, $diemden, $ngaydi, $songuoi, $page)
+		{
+			include $_SERVER['DOCUMENT_ROOT'].'/Banvemaybay/src-vs1/app/model/ChangBayModel.php';
+			include $_SERVER['DOCUMENT_ROOT'].'/Banvemaybay/src-vs1/app/model/ChuyenBayModel.php';
+			include $_SERVER['DOCUMENT_ROOT'].'/Banvemaybay/src-vs1/app/controller/changbay.php';
+			
+			$mdlChangBay = new ChangBayModel();
+			$mdlChuyenBay = new ChuyenBayModel();
+			$dsChuyenBay = array();
+			$machang = $mdlChangBay->getMabyDiaDiem($diemdi, $diemden);
+            $tongtrang = $mdlChuyenBay->getListCount($machang, $ngaydi, $songuoi, $page);
+			$result = $mdlChuyenBay->getListby($machang, $ngaydi, $songuoi, $page);
+			while ($row = $result->fetch_assoc()) {
+				array_push($dsChuyenBay, $row);
+			}
+
+			// Display result
+			$this->views("index_v",'chuyenbay_v', [
+				'dsChuyenBay' => $dsChuyenBay,
+				'diemdi' => $diemdi,
+				'diemden' => $diemden,
+				'ngaydi' => $ngaydi,
+				'songuoi' => $songuoi,
+				'trang' => $page,
+				'tongtrang' => $tongtrang
+			]);
         }
     }
 ?>
