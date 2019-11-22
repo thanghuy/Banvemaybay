e4cd29d49543479c560d2b05d64bcd0fc46572fa
Ninh Ngoc Hieu <30309069+ninhngochieu@users.noreply.github.com>
2019-11-22 16:37:48 +0700

    megre

---
+++ src-vs1/app/views/Pages/chitietdonhang_v.php
@@ -0,0 +1,97 @@
+<div>
+    <div class="container">
+        <form>
+            <div class="form-row">
+                <div class="col-md-12">
+                    <div class="form-row">
+                        <div class="col-md-3">
+                            <div class="jumbotron">
+                                <section id="vertical-menu">
+                                    <div class="list-group"><a class="list-group-item" href="./thongtinchitiet/thongtinKH"> <i class="fa fa-user fa-fw"></i>&nbsp;Thông tin khách hàng</a><a class="list-group-item" href="/thongtinchitiet/DH"> <i class="fa fa-list-alt fa-fw"></i>&nbsp;Xem đơn hàng</a><a class="list-group-item" href="#"> <i class="fa fa-sign-out fa-fw"></i>&nbsp;Đăng xuất</a></div>
+                                </section>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <form>
+                                <div class="form-row">
+                                    <div class="col">
+                                        <div class="jumbotron">
+                                            <div class="table-responsive">
+                                                <table class="table">  
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>Hãng chuyên chở</td>
+                                                            <td>Chuyến bay</td>
+                                                            <td>Ngày</td>
+                                                            <td>Từ</td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>VietJet Air</td>
+                                                            <td></td>
+                                                            <td>Thương gia</td>
+                                                            <td>MGFC01</td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td></td>
+                                                            <td></td>
+                                                            <td></td>
+                                                            <td></td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>Họ và tên</td>
+                                                            <td>Nguyen</td>
+                                                            <td>Thương gia</td>
+                                                            <td>MGFC01</td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>Nguyen Van</td>
+                                                            <td>Nguyen</td>
+                                                            <td>Thương gia</td>
+                                                            <td>MGFC01</td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>Nơi đi</td>
+                                                            <td>Nguyen</td>
+                                                            <td>Thương gia</td>
+                                                            <td>MGFC01</td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>Hà Nội</td>
+                                                            <td>Nguyen</td>
+                                                            <td>Thương gia</td>
+                                                            <td>MGFC01</td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>Nơi đến</td>
+                                                            <td>Nguyen</td>
+                                                            <td>Thương gia</td>
+                                                            <td>MGFC01</td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>TP HCM</td>
+                                                            <td>Nguyen</td>
+                                                            <td>Thương gia</td>
+                                                            <td>MGFC01</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
---
+++ src-vs1/app/views/Pages/xemdonhang_v.php
@@ -0,0 +1,76 @@
+<div>
+    <div class="container">
+        <form>
+            <div class="form-row">
+                <div class="col-md-12">
+                    <div class="form-row">
+                        <div class="col-md-3">
+                            <div class="jumbotron">
+                                <section id="vertical-menu">
+                                    <div class="list-group"><a class="list-group-item" href="./thongtinchitiet/thongtinKH"> <i class="fa fa-user fa-fw"></i>&nbsp;Thông tin khách hàng</a><a class="list-group-item" href="/thongtinchitiet/DH"> <i class="fa fa-list-alt fa-fw"></i>&nbsp;Xem đơn hàng</a><a class="list-group-item" href="#"> <i class="fa fa-sign-out fa-fw"></i>&nbsp;Đăng xuất</a></div>
+                                </section>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <form>
+                                <div class="form-row">
+                                    <div class="col">
+                                        <div class="jumbotron">
+                                            <div class="form-row">
+                                                <div class="col">
+                                                    <div id="timkiem_hd" class="input-group">
+                                                        <div class="input-group mb-3">
+                                                            <input type="text" id="search_input" class="form-control" placeholder="Tìm kiếm theo mã hoá đơn" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                                                            <div class="input-group-append">
+                                                                <button id="btnS" class="btn btn-secondary" type="button">Tìm kiếm</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col"></div>
+                                            </div>
+                                            <div class="col">
+                                            </div>
+                                            <div class="table-responsive">
+                                                <table class="table">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Mã hoá đơn</th>
+                                                            <th>Ngày đặt vé</th>
+                                                            <th>Số lượng</th>
+                                                            <th>Thành tiền</th>
+                                                            <th>&nbsp;Thanh toán</th>
+                                                            <th>Chi tiết</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody style="/*border: 1px solid;*/">
+                                                        <tr class="table_hd">
+                                                            <td>HD001</td>
+                                                            <td>2018-12-13</td>
+                                                            <td>3</td>
+                                                            <td>1840000đ</td>
+                                                            <td>ATM</td>
+                                                            <td><a href="#">Chi tiết</a></td>
+                                                        </tr>
+                                                        <tr class="table_hd">
+                                                            <td>HD002</td>
+                                                            <td>2018-02-14</td>
+                                                            <td>2</td>
+                                                            <td>20000000đ</td>
+                                                            <td>VISA</td>
+                                                            <td><a href="#">Chi tiết</a></td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
